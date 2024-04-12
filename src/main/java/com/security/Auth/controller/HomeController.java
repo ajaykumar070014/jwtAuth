ea0c2b46bd1bf30ef761529ada1323d881b98da9
Ajay Kumar <ajaykumar@newstreettech.com>
2024-04-12 18:33:20 +0530

    Login Validate using username and password

--- src/main/java/com/security/Auth/controller/HomeController.java
+++ src/main/java/com/security/Auth/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -14,9 +15,13 @@ import java.util.List;
 public class HomeController {
     @Autowired
     private UserService userService;
-    @GetMapping("users")
+    @GetMapping("/users")
     public List<UserModel> getUser(){
         System.out.println("Getting Users");
         return this.userService.getUsers();
+    }
+    @GetMapping("/current-user")
+    public String getLoggedInUser(Principal principal){
+        return principal.getName();
     }
 }
